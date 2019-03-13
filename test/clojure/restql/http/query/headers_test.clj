7f1965bf6a4892d04357634c60cd14da8bdd43c0
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2019-03-13 12:23:08 -0300

    fix(headers): rename header-allowed method

--- test/clojure/restql/http/query/headers_test.clj
+++ test/clojure/restql/http/query/headers_test.clj
@@ -4,4 +4,4 @@
 
 (deftest header-allowed-test
   (is (= {"restql-query-control" "ad-hoc", "user-agent" "insomnia/6.3.2", "teste" "teste", "accept" "*/*"}
-         (headers/header-allowed? {:type :ad-hoc} {:headers {"host" "localhost:9000", "user-agent" "insomnia/6.3.2", "content-length" "96", "teste" "teste", "accept" "*/*"}}))))
+         (headers/header-allowed {:type :ad-hoc} {:headers {"host" "localhost:9000", "user-agent" "insomnia/6.3.2", "content-length" "96", "teste" "teste", "accept" "*/*"}}))))
