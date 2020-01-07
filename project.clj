bd49e4645bd77256fc9b0514f55e1c90028b48b7
cupello <rafacupello@gmail.com>
2020-01-07 16:51:23 -0300

    bump version to 3.5.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject restql-http "v3.5.5" :description "restQL HTTP"
+(defproject restql-http "v3.5.6" :description "restQL HTTP"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://restql.b2w.io"}
