576a237ac365228d6a421c9b30b13bf6e37661be
Caio Ferreira <caiorcferreira@gmail.com>
2020-02-03 15:40:39 -0300

    refactor(cache): move cache implementation based on core.memoize to separated file

--- src/clojure/restql/http/cache/core.clj
+++ src/clojure/restql/http/cache/core.clj
@@ -1,20 +1,4 @@
 (ns restql.http.cache.core
-  (:require [environ.core :refer [env]]
-            [clojure.core.memoize :as memo]))
+  (:require [restql.http.cache.memoize :as memoize]))
 
-(def DEFAULT_TTL (if (contains? env :cache-ttl) (read-string (env :cache-ttl)) 60000))
-
-(def DEFAULT_CACHED_COUNT (if (contains? env :cache-count) (read-string (env :cache-count)) 2000))
-
-(defmulti cached
-  "Verifies if a given function is cached, executing and saving on the cache
-   if not cached or returning the cached value"
-  (fn [type & _] type))
-
-(defmethod cached :ttl
-  ([_:ttl function] (cached :ttl DEFAULT_TTL function))
-  ([_:ttl ttl function] (memo/ttl function {} :ttl/threshold ttl)))
-
-(defmethod cached :fifo
-  ([_:fifo function] (cached :fifo DEFAULT_CACHED_COUNT function))
-  ([_:fifo cached_count function] (memo/fifo function {} :fifo/threshold cached_count)))
+(def cached memoize/cached)
---
+++ src/clojure/restql/http/cache/memoize.clj
@@ -0,0 +1,20 @@
+(ns restql.http.cache.memoize
+  (:require [environ.core :refer [env]]
+            [clojure.core.memoize :as memo]))
+
+(def DEFAULT_TTL (if (contains? env :cache-ttl) (read-string (env :cache-ttl)) 60000))
+
+(def DEFAULT_CACHED_COUNT (if (contains? env :cache-count) (read-string (env :cache-count)) 2000))
+
+(defmulti cached
+          "Verifies if a given function is cached, executing and saving on the cache
+           if not cached or returning the cached value"
+          (fn [type & _] type))
+
+(defmethod cached :ttl
+  ([_:ttl function] (cached :ttl DEFAULT_TTL function))
+  ([_:ttl ttl function] (memo/ttl function {} :ttl/threshold ttl)))
+
+(defmethod cached :fifo
+  ([_:fifo function] (cached :fifo DEFAULT_CACHED_COUNT function))
+  ([_:fifo cached_count function] (memo/fifo function {} :fifo/threshold cached_count)))
